ed70ab731ea059bae96964becf35970689afebe3
riri <christopher.belaos@gmail.com>
2023-04-19 02:33:33 +0000

    Initial commit

--- src/COMPONENT/UI/results/image/sprite.com.ui.results.image.jsx
+++ src/COMPONENT/UI/results/image/sprite.com.ui.results.image.jsx
@@ -1,12 +1,12 @@
 import * as React from "react";
-import GetNokempo from "COMPONENT/LIBRARY/API/nokempo-fetch.com.lib.api";
+// import GetNokempo from "COMPONENT/LIBRARY/API/nokempo-fetch.com.lib.api";
 import { GlobalContainerContext } from "SETUP/app-context-manager/globals-container.setup";
 
 const DisplayNokempoSprite = ({ nokempoNem }) => {
   const { nokempoInamge } = React.useContext(GlobalContainerContext);
   return (
     <div>
-      <GetNokempo nokempoNem={nokempoNem} />
+      {/* <GetNokempo nokempoNem={nokempoNem} /> */}
       {/* <img alt={nokempoInamge} src={nokempoInamge} /> */}
     </div>
   );
